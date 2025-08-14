bc4aa9bcd0403efc106542a5112e61e0bfe86690
endrew444 <endrewmachado59@gmail.com>
2025-08-14 16:58:12 -0300

    14/08/25

--- ApiWebService/json_path_provider/lib/main.dart
+++ ApiWebService/json_path_provider/lib/main.dart
@@ -1,14 +1,15 @@
-import 'dart:convert';
-import 'dart:io';
 
-import 'package:flutter/cupertino.dart';
+import 'dart:convert';
+import 'dart:io'; //biblioteca interna do dart - busca os diretórios da aplicação
+
 import 'package:flutter/material.dart';
 import 'package:path_provider/path_provider.dart';
 
-void main() => runApp(ProdutoPage()); //uso do callback
+void main() => runApp(MaterialApp(home: ProdutoPage(),)); //uso do callback
 
+//classe que chama a mudança de estado
 class ProdutoPage extends StatefulWidget{
-  //constructor
+  //construtor
   const ProdutoPage({super.key});
 
   @override
@@ -17,11 +18,12 @@ class ProdutoPage extends StatefulWidget{
   }
 }
 
-//claqss que constroi a tela com as mudanças
+//classe que constroi a tela com as mudanças
 class _ProdutoPageState extends State<ProdutoPage>{
+  //atributos
   //lista de coleções
   List<Map<String,dynamic>> _produtos = [];
-  //controladores Edição de campo de texto
+  //controladores Edição de campos de texto
   final TextEditingController _nomeProdutoController = TextEditingController();
   final TextEditingController _valorProdutoController = TextEditingController();
 
@@ -36,13 +38,13 @@ class _ProdutoPageState extends State<ProdutoPage>{
   //path_provider (permite buscar arquivos do Dispositivo local)
   //instalar o package do pathprovider - pub add
   Future<File> _getFile() async{
-    final dir = await getApplicationCacheDirectory();
+    final dir = await getApplicationDocumentsDirectory();
     return File("${dir.path}/produtos.json");
   }
 
-void _carregarProdutos() async{
+  void _carregarProdutos() async{
     //tratamento de erros
-   try {
+    try {
       final file  = await _getFile();
       String jsonProdutos = await file.readAsString();
       List<dynamic> dados = json.decode(jsonProdutos);
@@ -65,8 +67,9 @@ void _carregarProdutos() async{
       print("Erro ao salvar Produtos $e");
     }
   }
-//adiciaonar proutos na lista
-  void adicionarProduto() {
+
+  //adiciaonar proutos na lista
+  void _adicionarProduto() {
     String nome = _nomeProdutoController.text.trim();
     String valorStr = _valorProdutoController.text.trim();
     if(nome.isEmpty || valorStr.isEmpty) return; // interrompe o método -> caso algum vazio
@@ -94,7 +97,47 @@ void _carregarProdutos() async{
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      appBar: AppBar(title: Text("Cadastro de Produtos"),),
+      body: Padding(
+        padding: EdgeInsets.all(16),
+        child: Column(
+
+          children: [
+            TextField(
+              controller: _nomeProdutoController,
+              decoration: InputDecoration(labelText: "Nome do Produto"),
+            ),
+            TextField(
+              controller: _valorProdutoController,
+              decoration: InputDecoration(labelText: "Valor do Produto (ex: 15.55)"),
+              keyboardType: TextInputType.numberWithOptions(decimal: true),
+            ),
+            SizedBox(height: 10,),
+            ElevatedButton(
+              onPressed: _adicionarProduto, 
+              child: Text("Adicionar Produto")),
+            Divider(),
+            Expanded(
+              //operador Ternário (if/else curto)
+              child: _produtos.isEmpty ?
+              Center(child: Text("Nenhum Produto Cadastrado"),) :
+              ListView.builder(
+                itemCount: _produtos.length,
+                itemBuilder: (context,index){
+                  final produto = _produtos[index];
+                  return ListTile(
+                    title: Text(produto["nome"]),
+                    subtitle: Text("R\$ ${produto["valor"]}.00"),//coloca 2 casas decimais
+                    trailing: IconButton(
+                      onPressed: () => _removerProduto(index), 
+                      icon: Icon(Icons.delete)),
+                  );
+                })
+              ),
+
+          ],
+        )
+      )
     );
   }
 }
