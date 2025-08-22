96cef6bccbbfbc9c7fc078de6b6f72b1a7faefb8
endrew444 <endrewmachado59@gmail.com>
2025-08-22 10:15:01 -0300

    Unindo todos os projetos em um repositório só

--- mobile_curso/ApiWebService/biblioteca_app/lib/services/api_service.dart
+++ mobile_curso/ApiWebService/biblioteca_app/lib/services/api_service.dart
@@ -1,6 +1,14 @@
 // classe de ajuda para conexão com API
+import 'dart:convert';
 
+import 'package:http/http.dart' as http;
 class ApiService {
   //base url pra conexão
   String _baseUrl = "http://10.109.197.8:3011";
+
+  //metodos static(metodfos da classse e n do obj)
+  //GET -> listar todos os recursos
+  static Future<List<dynamic>> getList(String path) async{
+    final res = await http.get(Uri.parse("$_baseUrl/$path"));
+    if(res.statusCode ==200) return json.decode(res.body);
 }
