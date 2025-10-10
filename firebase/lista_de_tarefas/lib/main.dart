63d838a0e665d54598101448d650f38144604fe4
endrew444 <endrewmachado59@gmail.com>
2025-10-10 11:05:02 -0300

    10.10.25

--- firebase/lista_de_tarefas/lib/main.dart
+++ firebase/lista_de_tarefas/lib/main.dart
@@ -1,6 +1,5 @@
 
 import 'package:firebase_core/firebase_core.dart';
-import 'package:flutter/material.dart';
 import 'package:lista_de_tarefas/firebase_options.dart';
 import 'package:lista_de_tarefas/views/auth_view.dart';
 
